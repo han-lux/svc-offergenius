6e40c01c1d60e0086602951e8b23b03eedbb17ad
Hanif Shersy <hanif.shersy@luxuryescapes.com>
2023-04-27 11:29:57 +1000

    Add express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
+import express from "express";
+const port = process.env.PORT || 4000;
+
 export function startServer() {
-  // TODO
+  return app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
 }
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Endpoint")
+});
