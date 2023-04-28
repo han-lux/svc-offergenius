b1e4fa02a272263cf70ccdbdd2d035457a09131d
Jordi Kitto <jordi.kitto@luxuryescapes.com>
2023-04-28 10:13:58 +1000

    Added two query method

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,72 +1,21 @@
-export function offerGeniusPrompt(offer: string, hotelName: string) {
+export function offerGeniusPrompt(offer: string) {
     return `
-    You are an API at the luxury travel company named 'Luxury Escapes'. Your job is to read JSON data about hotel offers and answer questions using only the information contained in the JSON data.
+    You are an assistant at the luxury travel company named 'Luxury Escapes'. 
+    
+    Your job is to read JSON data about hotel offers and answer questions using only the information contained in the JSON data.
 
-    When you answer a question, provide the a sentence of the verbatim text in the JSON data that you used to determine this answer.
+    Always try your best to answer the question however you can.
 
-    Your response format is:
+    You respond succinctly, with only the relevant information. 
 
-    {
-        "answers":[
-            {
-                "response": string
-                "reference": string
-            }
-        ]
-    }
-
-    The following are some example questions followed by example responses.
-
-    ---
-
-    What is the name of the hotel
-    {
-        "answers": [
-            {
-                "response": "The hotel is named ${hotelName}",
-                "reference": "${hotelName}"
-            }
-        ]
-    }
-
-    Is airport transfer included?
-    {
-        "answers": [
-            {
-                "response": "No",
-                "reference": "Airport transfers are not included in your package, please plan accordingly."
-            },
-            {
-                "response": "Yes",
-                "reference": "Private Villa with included airport transfer."
-            }
-    ]
-    }
-
-    Do they sell anti-tank weaponry
-    {
-        "answers": [
-            {
-                "response": "I cannot answer that based on the current information we have.",
-                "reference": ""
-            }
-        ]
-    }
-
-    Question: Do you love me?
-    {
-        "answers": [
-            {
-                "response": "Sorry, I can only answer questions about Fraser Suites Sydney",
-                "reference": ""
-            }
-        ]
-    }
-
-    ---
-
-    Answer using the following JSON data and the information contained in this JSON data only (do not use the example data above), and then respond ONLY in the aforementioned JSON format.
-
+    Answer questions by summarizing the following JSON data only.
+    
     ${JSON.stringify(offer)}
+    `
+}
+
+export function offerGeniusQuotationPrompt(): string {
+    return `
+    Provide a quotation from the JSON data that proves your previous answer. If you can't find a quotation, then respond with No.
     `
 }
--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import { encode } from "gpt-3-encoder";
-import { offerGeniusPrompt } from "./prompts";
+import { offerGeniusPrompt, offerGeniusQuotationPrompt } from "./prompts";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -26,30 +26,46 @@ export class OfferGeniusService {
     question,
   }: CustomerSupportQuestion) {
     const offer = await this.fetchOffer(offerId);
+
     console.log("Answering query for: ", offer.name);
-    console.log("Using prompt: ", offerGeniusPrompt(offer, offer.name)); 
+    console.log("Using prompt: ", offerGeniusPrompt(offer.copy)); 
+
     const answer = await this.api.createChatCompletion({
       model: "gpt-3.5-turbo",
-      temperature: 0,
+      temperature: 0.7,
       messages: [
         {
           role: "system",
-          content: offerGeniusPrompt(offer.copy, offer.name),
+          content: offerGeniusPrompt(offer.copy),
         },
         { role: "user", content: question },
       ],
     })
 
-    console.log(answer.data.choices[0].message?.content)
+    const answerContent = answer.data.choices[0].message!.content
 
-    try {
-      return JSON.parse(answer.data.choices[0].message!.content).answers;
-    } catch (error) {
-      console.log(error)
-      return {
-        answer: "Error: ", error
-      };
-    }
+    const quotation = await this.api.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      temperature: 0.7,
+      messages: [
+        { role: "system", content: offerGeniusPrompt(offer.copy) },
+        { role: "user", content: question },
+        { role: "assistant", content: answerContent },
+        { role: "user", content: offerGeniusQuotationPrompt() }
+      ],
+    })
+
+    console.log(quotation.data.choices)
+    const quotationContent = quotation.data.choices[0].message!.content
+
+    return {
+      answers: [
+        {
+          answer: answerContent,
+          reference: quotationContent.toLocaleLowerCase() === "no." ? null : quotationContent
+        }
+      ]
+    };
   }
 
   countToken(message: string): number {
